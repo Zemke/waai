c8d62f9360f8a5a713066f558f45c44549166b4a
Zemke <florian@zemke.io>
2021-12-11 21:49:58 +0100

    Don't need label annots

--- labelling/crop.py
+++ labelling/crop.py
@@ -6,21 +6,19 @@ from PIL import Image
 import sys
 import time
 
-with open('./annot.csv', 'w') as csvf:
-  with os.scandir("target") as it:
-    for entry in it:
-      if entry.is_file() and entry.name.lower().endswith('.json'):
-        with open(entry.path, 'r') as f:
-          j = json.loads(f.read())
-        for doc in j:
-          img = Image.open("source/" + doc["image"])
-          for annot in doc["annotations"]:
-            coord = annot["coordinates"]
-            x, y, width, height = coord["x"], coord["y"], coord["width"], coord["height"]
-            crop = img.crop((x-width/2, y-height/2, x+width/2, y+height/2))
-            #crop.show()
-            ts = round(time.time() * 10000)
-            filen = f"crops/{annot['label']}/{doc['image'][:-4]}_{ts}.png"
-            crop.save(filen)
-            csvf.write(','.join([str(i) for i in [filen, annot['label'], x, y, width, height]]) + '\n')
+with os.scandir("target") as it:
+  for entry in it:
+    if entry.is_file() and entry.name.lower().endswith('.json'):
+      with open(entry.path, 'r') as f:
+        j = json.loads(f.read())
+      for doc in j:
+        img = Image.open("source/" + doc["image"])
+        for annot in doc["annotations"]:
+          coord = annot["coordinates"]
+          x, y, width, height = coord["x"], coord["y"], coord["width"], coord["height"]
+          crop = img.crop((x-width/2, y-height/2, x+width/2, y+height/2))
+          #crop.show()
+          ts = round(time.time() * 10000)
+          filen = f"crops/{annot['label']}/{doc['image'][:-4]}_{ts}.png"
+          crop.save(filen)
 
