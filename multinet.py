69190fc7854c06978ca74393cd808926e3ef7a8f
Zemke <florian@zemke.io>
2023-05-18 10:55:46 +0200

    sequential features and classifier

--- multinet.py
+++ multinet.py
@@ -19,26 +19,30 @@ class MultiNet(nn.Module):
   def __init__(self, num_classes):
     super().__init__()
     self.num_classes = num_classes
-
-    self.conv1 = nn.Conv2d(3, 10, 5)
-    self.pool = nn.MaxPool2d(2, 2)
-    self.conv2 = nn.Conv2d(10, 20, 3)
-    self.fc1 = nn.Linear(20 * 5 * 5, 300)
-    self.fc2 = nn.Linear(300, 200)
-    self.fc3 = nn.Linear(200, 100)
-    self.fc4 = nn.Linear(100, num_classes)
-
+    self.features = nn.Sequential(
+      nn.Conv2d(3, 10, 5),
+      nn.ReLU(inplace=True),
+      nn.MaxPool2d(2, 2),
+      nn.Conv2d(10, 20, 3),
+      nn.ReLU(inplace=True),
+      nn.MaxPool2d(2, 2),
+    )
+    self.classifier = nn.Sequential(
+      nn.Linear(20 * 5 * 5, 300),
+      nn.ReLU(inplace=True),
+      nn.Linear(300, 200),
+      nn.ReLU(inplace=True),
+      nn.Linear(200, 100),
+      nn.ReLU(inplace=True),
+      nn.Linear(100, num_classes),
+    )
     self.to(device)
     print("moved net to", device)
 
   def forward(self, x: torch.Tensor) -> torch.Tensor:
-    x = self.pool(F.relu(self.conv1(x)))
-    x = self.pool(F.relu(self.conv2(x)))
+    x = self.features(x)
     x = torch.flatten(x, 1)  # flatten all dimensions except batch
-    x = F.relu(self.fc1(x))
-    x = F.relu(self.fc2(x))
-    x = F.relu(self.fc3(x))
-    x = self.fc4(x)
+    x = self.classifier(x)
     return x
 
 
