642875840b4b9b5f086bce9a59e0fefb8b4da3ef
Zemke <flzemke@gmail.com>
2025-11-07 10:02:22 +0100

    transforms for paste imgs

--- dynamicset.py
+++ dynamicset.py
@@ -23,41 +23,49 @@ class DynamicSet(Dataset):
     self.transform = T.Compose([
       T.PILToTensor(),
       v2.ToDtype(torch.float32, scale=True),
-      # no norm in rcnn
-      #T.Normalize(torch.tensor([0.8580, 0.4778, 0.2055]), torch.tensor([0.8040, 0.4523, 0.2539]))
     ])
     self.S = [30, 25, 40, 25]
     self.M = [f for f in listdir("od/target") if isfile(join("od/target", f)) and f.split(".")[-1] == "png"]
-    self.W = [Image.open(c).convert("RGBA").resize((self.S[0], self.S[0])) for c in ['od/worms/' + f for f in listdir("od/worms") if isfile(join("od/worms", f)) and f.split(".")[-1] == "png"]]
-    self.C = [Image.open(c).convert("RGBA").resize((self.S[t+1], self.S[t+1])) for t,c in enumerate([
+    self.W = [Image.open(c).convert("RGBA") for c in ['od/worms/' + f for f in listdir("od/worms") if isfile(join("od/worms", f)) and f.split(".")[-1] == "png"]]
+    self.C = [Image.open(c).convert("RGBA") for t,c in enumerate([
       "od/weapons_alpha/mine.png",
       "od/weapons_alpha/barrel.png",
       "od/weapons_alpha/dynamite.png",
     ])]
+    self.transform_paste = T.Compose([
+      T.PILToTensor(),
+      v2.ToDtype(torch.float32, scale=True),
+      v2.RandomHorizontalFlip(p=.5),
+      v2.RandomRotation(360),
+      v2.RandomResize(15, 50),
+      v2.ToPILImage(),
+    ])
 
   def __len__(self):
     return self.length
 
   def __getitem__(self, idx):
     back_im = Image.open("od/target/" + self.M[randrange(len(self.M))]).convert("RGBA")
-    wi = 0
-    SP = 45
+    SP = 120
     height, width, _ = np.array(back_im).shape
     a = Image.new("RGBA", back_im.size, (0,0,0,0))
     boxes, labels = [], []
     for y in range(SP-30, height-SP, SP):
       for x in range(SP-30, width-SP, SP):
         im2 = self.W[randrange(len(self.W))] if (t := randrange(len(CLASSES))) == 0 else self.C[t-1]
-        wi += 1
+        im2 = self.transform_paste(im2)
         a.paste(im2, (x1 := x+randrange(10), y1 := y+randrange(10)))
         labels.append(t)
-        boxes.append([x1, y1, x1+self.S[t], y1+self.S[t]])
+        boxes.append([x1, y1, x1+im2.width, y1+im2.height])
     back_im = Image.alpha_composite(back_im, a).convert("RGB")
     return self.transform(back_im), {
       "boxes": torch.as_tensor(boxes, dtype=torch.float),
       "labels": torch.as_tensor(labels, dtype=torch.int64),
     }
 
+# TODO random paste
+# TODO transforms
+# TODO normalize
 
 def collate_fn(batch):
   return tuple(zip(*batch))
@@ -65,7 +73,7 @@ def collate_fn(batch):
 def load(dataset, batch_size):
   return DataLoader(
     dataset,
-    num_workers=2, persistent_workers=True,
+    #num_workers=8, persistent_workers=True,
     batch_size=batch_size, collate_fn=collate_fn)
 
 
