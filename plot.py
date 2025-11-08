5c458e69e649db65d6eab20d513cca21c52cab3e
Zemke <flzemke@gmail.com>
2025-11-08 18:59:21 +0100

    plot restart

--- plot.py
+++ plot.py
@@ -1,37 +1,3 @@
-y = """
-8 {'loss_classifier': tensor(0.1707, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1797, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1469, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0822, grad_fn=<DivBackward0>)}
-8 {'loss_classifier': tensor(0.1771, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1953, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1834, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.1001, grad_fn=<DivBackward0>)}
-9 {'loss_classifier': tensor(0.1483, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1777, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1282, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0928, grad_fn=<DivBackward0>)}
-9 {'loss_classifier': tensor(0.1480, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1743, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1435, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0828, grad_fn=<DivBackward0>)}
-9 {'loss_classifier': tensor(0.1544, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1778, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1125, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0846, grad_fn=<DivBackward0>)}
-9 {'loss_classifier': tensor(0.1641, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1904, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1448, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0850, grad_fn=<DivBackward0>)}
-9 {'loss_classifier': tensor(0.1614, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1813, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1442, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0948, grad_fn=<DivBackward0>)}
-9 {'loss_classifier': tensor(0.1450, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1849, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1460, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0844, grad_fn=<DivBackward0>)}
-9 {'loss_classifier': tensor(0.1664, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1962, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1397, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0826, grad_fn=<DivBackward0>)}
-10 {'loss_classifier': tensor(0.1774, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1829, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1393, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0816, grad_fn=<DivBackward0>)}
-10 {'loss_classifier': tensor(0.1578, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1868, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1411, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0867, grad_fn=<DivBackward0>)}
-10 {'loss_classifier': tensor(0.1462, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1610, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1295, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0865, grad_fn=<DivBackward0>)}
-10 {'loss_classifier': tensor(0.1525, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1824, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1451, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0846, grad_fn=<DivBackward0>)}
-10 {'loss_classifier': tensor(0.1663, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1714, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1219, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0899, grad_fn=<DivBackward0>)}
-10 {'loss_classifier': tensor(0.1746, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1871, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1306, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0850, grad_fn=<DivBackward0>)}
-10 {'loss_classifier': tensor(0.1344, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1625, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1396, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0819, grad_fn=<DivBackward0>)}
-11 {'loss_classifier': tensor(0.1500, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1617, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1209, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0809, grad_fn=<DivBackward0>)}
-11 {'loss_classifier': tensor(0.1596, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1742, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1393, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0881, grad_fn=<DivBackward0>)}
-11 {'loss_classifier': tensor(0.1597, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1932, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1431, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0866, grad_fn=<DivBackward0>)}
-11 {'loss_classifier': tensor(0.1639, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1887, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1447, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0844, grad_fn=<DivBackward0>)}
-11 {'loss_classifier': tensor(0.1542, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1784, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1398, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0884, grad_fn=<DivBackward0>)}
-11 {'loss_classifier': tensor(0.1574, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1781, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1314, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0835, grad_fn=<DivBackward0>)}
-11 {'loss_classifier': tensor(0.1776, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.2127, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1216, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0834, grad_fn=<DivBackward0>)}
-12 {'loss_classifier': tensor(0.1544, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1858, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1337, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0812, grad_fn=<DivBackward0>)}
-12 {'loss_classifier': tensor(0.1483, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1709, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1155, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0858, grad_fn=<DivBackward0>)}
-12 {'loss_classifier': tensor(0.1397, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1649, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1487, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0843, grad_fn=<DivBackward0>)}
-0 {'loss_classifier': tensor(0.1593, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1819, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1547, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0924, grad_fn=<DivBackward0>)}
-0 {'loss_classifier': tensor(0.1624, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1844, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1374, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0892, grad_fn=<DivBackward0>)}
-0 {'loss_classifier': tensor(0.1524, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1637, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1401, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0833, grad_fn=<DivBackward0>)}
-0 {'loss_classifier': tensor(0.1407, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1592, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1305, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0814, grad_fn=<DivBackward0>)}
-0 {'loss_classifier': tensor(0.1492, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1648, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.1333, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.0835, grad_fn=<DivBackward0>)}
-""".strip().split("\n")
-
 import re
 import numpy as np
 import matplotlib.pyplot as plt
@@ -46,8 +12,8 @@ plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, hspace = 0, wspace
 plt.margins(0,0)
 plt.gca().xaxis.set_major_locator(plt.NullLocator())
 plt.gca().yaxis.set_major_locator(plt.NullLocator())
-#fig, ax = plt.subplots()
 
+y = []
 G = L = None
 while True:
   with open('train.log', 'r') as f:
@@ -55,9 +21,11 @@ while True:
       if re.match("^[0-9]+ ", line) is None:
         continue
       if line not in y:
-        print('plot', line)
+        print('plt', line)
         y.append(line)
-  z = [[float(n) for n in re.sub("[^0-9.]+", ",", re.sub('^[0-9]+ ', '', y1)).replace(",0,", ",")[1:-1].split(",")] for y1 in y[-2_000:]]
+  if len(y) < 1:
+    continue
+  z = [[float(n) for n in re.sub("[^0-9.]+", ",", re.sub('^[0-9]+ ', '', y1)).replace(",0,", ",")[1:-1].split(",")] for y1 in y]
   zt = np.transpose(z)
   labels = re.findall("'(.*?)'", y[0])
   if G is None:
@@ -66,7 +34,8 @@ while True:
   for i,l in enumerate(labels):
     if G[i] is not None:
       G[i][0].remove()
-    G[i] = plt.plot(moving_average(zt[i], 15), label=l, color=colors[i])
+    #G[i] = plt.plot(moving_average(zt[i], 15), label=l, color=colors[i])
+    G[i] = plt.plot(zt[i], label=l, color=colors[i])
   if L is not None:
     L.remove()
   L = plt.legend([g for g, in G], labels)
