acc0e5a136f64966f5fe908fcf4260329918e563
Zemke <flzemke@gmail.com>
2025-11-11 22:34:02 +0100

    parameterized plot

--- plot.py
+++ plot.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python3
+
+import os
 import re
+from collections import deque
+
 import numpy as np
 import matplotlib.pyplot as plt
-from collections import deque
 
 def moving_average(x, w):
   return np.convolve(x, np.ones(w), 'valid') / w
@@ -14,9 +18,10 @@ plt.margins(0,0)
 plt.gca().xaxis.set_major_locator(plt.NullLocator())
 plt.gca().yaxis.set_major_locator(plt.NullLocator())
 
-y = deque([], 500)
+
+y = deque([], int(os.getenv("SIZE", 1_000)))
 G = L = None
-read = 0
+read = int(os.getenv("READ", 0))
 while True:
   y_bef = len(y)
   with open('train.log', 'r') as f:
@@ -33,8 +38,8 @@ while True:
             continue
           l = re.sub("device.+?',", '', fa1)
           print('plt', l)
-          y.append(l)
-          new = True
+          if read % int(os.getenv("STEP", 2)) == 0:
+            y.append(l)
         buff = ''
   if y_bef == len(y):
     continue
@@ -42,15 +47,14 @@ while True:
     continue
   z = [[float(n) for n in re.sub("[^0-9.]+", ",", y1).replace(",0,", ",")[1:-1].split(",")] for y1 in y]
   zt = np.transpose(z)
-  labels = re.findall("'(.*?)'", y[0])
+  labels = [f"{v} {k}" for k,v in zip(re.findall("'(.*?)'", y[-1]), z[-1])]
   if G is None:
     G = [None] * len(labels)
-  colors = ['magenta', 'cyan', 'lime', 'yellow', 'red', 'orange', 'blue', 'green'][:len(labels)]
+  colors = ['magenta', 'yellow', 'cyan', 'lime', 'red', 'orange', 'blue', 'green'][:len(labels)]
   for i,l in enumerate(labels):
     if G[i] is not None:
       G[i][0].remove()
-    #G[i] = plt.plot(moving_average(zt[i], 15), label=l, color=colors[i])
-    G[i] = plt.plot(zt[i], label=l, color=colors[i])
+    G[i] = plt.plot(moving_average(zt[i], int(len(y)*(os.getenv("MA", 1/40)))), label=l, color=colors[i])
   if L is not None:
     L.remove()
   L = plt.legend([g for g, in G], labels)
